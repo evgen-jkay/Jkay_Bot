28b309ece6f286432dd72dc8b5ea6ce9003580fb
evgen-jkay <evgenlandarenko@gmail.com>
2021-11-10 17:58:16 +0200

    #5 много

---
+++ src/main/java/bots/updateshandlers/CommandsHandler.java
@@ -0,0 +1,67 @@
+package bots.updateshandlers;
+
+import bots.BotConfig;
+import bots.commands.StartCommand;
+import bots.commands.StopCommand;
+import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.logging.BotLogger;
+
+/**
+ * This handler mainly works with commands to demonstrate the Commands feature of the API
+ *
+ * @author Timo Schulz (Mit0x2)
+ */
+public class CommandsHandler extends TelegramLongPollingCommandBot {
+
+    public static final String LOGTAG = "COMMANDSHANDLER";
+
+    /**
+     * Constructor.
+     */
+    public CommandsHandler(String botUsername) {
+        super(botUsername);
+
+        register(new StartCommand());
+        register(new StopCommand());
+
+        registerDefaultAction((absSender, message) -> {
+            SendMessage commandUnknownMessage = new SendMessage();
+            commandUnknownMessage.setChatId(message.getChatId());
+            commandUnknownMessage.setText("The command '" + message.getText() + "' is not known by this bot. Here comes some help ");
+            try {
+                absSender.execute(commandUnknownMessage);
+            } catch (TelegramApiException e) {
+                BotLogger.error(LOGTAG, e);
+            }
+        });
+    }
+
+    @Override
+    public void processNonCommandUpdate(Update update) {
+
+        if (update.hasMessage()) {
+            Message message = update.getMessage();
+
+            if (message.hasText()) {
+                SendMessage echoMessage = new SendMessage();
+                echoMessage.setChatId(message.getChatId());
+                echoMessage.setText("Hey heres your message:\n" + message.getText());
+
+                try {
+                    execute(echoMessage);
+                } catch (TelegramApiException e) {
+                    BotLogger.error(LOGTAG, e);
+                }
+            }
+        }
+    }
+
+    @Override
+    public String getBotToken() {
+        return BotConfig.BOT_TOKEN;
+    }
+}
