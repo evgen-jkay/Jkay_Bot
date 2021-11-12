672cc6ccacb24a769395688637a79b0fb421e056
evgen-jkay <evgenlandarenko@gmail.com>
2021-11-12 23:12:58 +0200

    Чистка

--- src/main/java/bots/JkayBot.java
+++ src/main/java/bots/JkayBot.java
@@ -1,7 +1,9 @@
 package bots;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 /**
  * @author Eugen Landarenko
@@ -19,7 +21,18 @@ public class JkayBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        update.getUpdateId();
 
+        SendMessage sendMessage = new SendMessage().setChatId(update.getMessage().getChatId());
+
+        if (update.getMessage().getText().equals("Привет")) {
+            sendMessage.setText("Привет дружище!");
+            try {
+                execute(sendMessage);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
 
--- src/main/java/bots/Main.java
+++ src/main/java/bots/Main.java
@@ -2,14 +2,20 @@ package bots;
 
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 /**
  * @author Eugen Landarenko
  */
 public class Main {
-
     public static void main(String[] args) {
         ApiContextInitializer.init();
         TelegramBotsApi telegram = new TelegramBotsApi();
+
+        try {
+            telegram.registerBot(new JkayBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
