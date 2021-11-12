aba4e6ec9c166d8d8947cb2b4c15fa0d97c18ee2
evgen-jkay <evgenlandarenko@gmail.com>
2021-11-12 22:33:19 +0200

    Чистка

--- src/main/java/bots/JkayBot.java
+++ src/main/java/bots/JkayBot.java
@@ -1,9 +1,7 @@
 package bots;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class JkayBot extends TelegramLongPollingBot {
     @Override
@@ -18,17 +16,7 @@ public class JkayBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            SendMessage message = new SendMessage();
-            message.setChatId(update.getMessage().getChatId().toString());
-            message.setText(update.getMessage().getText());
 
-            try {
-                execute(message);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
-        }
     }
 }
 
